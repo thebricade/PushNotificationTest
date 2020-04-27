8dd781c3631ad969758ad1acd361dfd300b7f654
Brianna <bs3864@nyu.edu>
2020-04-27 06:45:42 -0400

    Testing Push

---
+++ iOSNotifications/Assets/Scripts/iOSNotifications.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Notifications.iOS;
+using UnityEngine;
+
+public class iOSNotifications : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var timeTrigger = new iOSNotificationTimeIntervalTrigger()
+        {
+            TimeInterval = new TimeSpan(0, 3,0),
+            Repeats = false
+        };
+
+        var notification = new iOSNotification()
+        {
+            // You can optionally specify a custom identifier which can later be 
+            // used to cancel the notification, if you don't set one, a unique 
+            // string will be generated automatically.
+            Identifier = "_notification_01",
+            Title = "Title",
+            Body = "Scheduled at: " + DateTime.Now.ToShortDateString() + " triggered in 5 seconds",
+            Subtitle = "This is a subtitle, something, something important...",
+            ShowInForeground = true,
+            ForegroundPresentationOption = (PresentationOption.Alert | PresentationOption.Sound),
+            CategoryIdentifier = "category_a",
+            ThreadIdentifier = "thread1",
+            Trigger = timeTrigger,
+        };
+
+        iOSNotificationCenter.ScheduleNotification(notification);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ButtonRequestAuthorization()
+    {
+        StartCoroutine(RequestAuthorization());
+    }
+
+
+    IEnumerator RequestAuthorization()
+    {
+        using (var req = new AuthorizationRequest(AuthorizationOption.Alert | AuthorizationOption.Badge, true))
+        {
+            while (!req.IsFinished)
+            {
+                yield return null;
+            }
+
+            ;
+
+            string res = "\n RequestAuthorization: \n";
+            res += "\n finished: " + req.IsFinished;
+            res += "\n granted :  " + req.Granted;
+            res += "\n error:  " + req.Error;
+            res += "\n deviceToken:  " + req.DeviceToken;
+            Debug.Log(res);
+        }
+    }
+    
+    
+    
+}
